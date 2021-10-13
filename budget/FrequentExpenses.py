6e0572e765775cbc75690d412793b943583c0fa0
Meghan Pund <meghan.pund@gmail.com>
2021-10-13 12:16:53 -0400

    trying to get this to work with pluralsight. updated my pip

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,5 +1,5 @@
 import collections
-
+import matplotlib.pyplot  as plt
 import Expense
 
 expenses = Expense.Expenses()
@@ -12,4 +12,13 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 
-print(spending_counter)
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(*top5)
+
+fig, ax = plt.subplots()
+
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+
+plt.show()
