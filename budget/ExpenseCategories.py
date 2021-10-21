8c2cb2ec63f99a251efa60282005a7176b9441cb
Meghan Pund <meghan.pund@gmail.com>
2021-10-20 21:53:10 -0400

    !

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -12,7 +12,8 @@ def main():
         print("Sets are NOT equal by == test")
 
     for a,b in zip(divided_for_loop, divided_set_comp):
-        if not a.issubset(b) and not b.issubset(a):
+        if not (a.issubset(b) and
+            b.issubset(a)):
             print("Sets are NOT equal by subset test.")
 
     print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
